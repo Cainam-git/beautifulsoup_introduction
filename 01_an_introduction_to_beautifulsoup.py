275ea1c7d2bcd90affae0723a94cd6614e4a4b22
Cainam <git@cainam.net>
2020-09-19 12:43:40 +0200

    commit

--- 01_an_introduction_to_beautifulsoup.py
+++ 01_an_introduction_to_beautifulsoup.py
@@ -4,11 +4,11 @@ from urllib.error import HTTPError, URLError
 
 if __name__ == '__main__':
     try:
-        html = urlopen('http://www.pythonscraping.com/pages/page1.html')
+        html = urlopen('http://www.pthonscraping.com/pages/page1.html')
     except HTTPError as e:
         print(e)
     except URLError as e:
-        print(e)
+        print("The server could not be found.")
     else:
         bs = BeautifulSoup(html, 'lxml')
         print(bs.h1)
